eaffc70b27958427d31815b8f0c26cb81913e3d5
lzmouh <lzmouh@gmail.com>
2025-07-05 18:17:27 +0300

    Update NMTD-Simulator.py

--- NMTD-Simulator.py
+++ NMTD-Simulator.py
@@ -99,7 +99,7 @@ if page == "Simulator":
             )
 
     config["total_thickness"] = sum([layer[1] for layer in config["layer_data"]])
-    st.markdown(f"**📏 Total Pipe Thickness: `{config[total_thickness]:.2f}` inches**")
+    st.write(f"**📏 Total Pipe Thickness: `{config[total_thickness]:.2f}` inches**")
 
     st.subheader("📌 Defect Settings")
     c1, c2 = st.columns(2)
